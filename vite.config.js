9e7f983c1b9f93430ab9f9e2b24b1105a4626dab
esdras4757 <esdras4757@gmail.com>
2025-01-15 19:30:49 -0600

    Actualizar el script de inicio en package.json y configurar el servidor en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: process.env.PORT || 3000,
+    host: true,
+  },
   define: {
     'process.env': process.env
   },
